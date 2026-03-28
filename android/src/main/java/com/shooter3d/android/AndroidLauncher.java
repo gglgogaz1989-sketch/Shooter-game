64c4cfc005cfeb6d526204a2804496db9ac0ca3a
gglgog <gglgogaz1989@gmail.com>
2026-03-28 17:30:21 +0200

    Update AndroidLauncher.java

--- android/src/main/java/com/shooter3d/android/AndroidLauncher.java
+++ android/src/main/java/com/shooter3d/android/AndroidLauncher.java
@@ -1,17 +1,24 @@
 package com.shooter3d.android;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import androidx.core.content.FileProvider;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.shooter3d.GameMain;
+import java.io.File;
 
 public class AndroidLauncher extends AndroidApplication {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // иммерсивный режим
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -30,5 +37,36 @@ public class AndroidLauncher extends AndroidApplication {
         config.useWakelock = true;
         
         initialize(new GameMain(), config);
+    }
+    
+    // Метод для установки APK, вызываемый из Updater
+    public static void installApk(String filePath) {
+        try {
+            AndroidLauncher instance = (AndroidLauncher) com.badlogic.gdx.Gdx.app.getApplicationListener();
+            instance.installApkInternal(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void installApkInternal(String filePath) {
+        File apkFile = new File(filePath);
+        if (!apkFile.exists()) {
+            return;
+        }
+        
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri apkUri;
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            apkUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", apkFile);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            apkUri = Uri.fromFile(apkFile);
+        }
+        
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
